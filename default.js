3dc2a801b9278a4754d5919232a0a8aae9015ea3
Andreas Wålm <andreas@walm.net>
2011-10-12 21:34:02 +0200

    Remove some ads

--- default.js
+++ default.js
@@ -1,2 +1,9 @@
 // this will be run on every site
 console.log('DotJS is on');
+
+// remove ads
+var ads = $('iframe[id*="aswift"], .ads');
+if (ads.length > 1) {
+  console.log("DotJS: Removing ads", ads);
+  ads.remove();
+}
